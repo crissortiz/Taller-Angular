7758e4990a8a1e85a9356f9bebbc279806382e98
Cristian Ortiz <cd.ortizs12@uniandes.edu.co>
2025-04-21 09:51:11 -0500

    creacion de data,series y main

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,63 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+import { series } from './data';
+
 
 platformBrowserDynamic().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true,
 })
   .catch(err => console.error(err));
+
+const seriesTable: HTMLElement = document.getElementById('seriesTable')!;
+
+showSeries(series);
+
+function showSeries(series: any[]): void {
+    const seriesList: HTMLElement = document.createElement('tbody');
+    let totalSeasons = 0;
+
+    series.forEach(serie => {
+        const serieRow: HTMLElement = document.createElement('tr');
+        serieRow.innerHTML = `
+            <td class="series-id">${serie.getId()}</td>
+            <td class="series-name">
+                <a href="#" class="serie-link">${serie.getName()}</a>
+            </td>
+            <td>${serie.getChannel()}</td>
+            <td>${serie.getSeasons()}</td>
+        `;
+
+        const linkElement = serieRow.querySelector('.serie-link') as HTMLAnchorElement;
+        linkElement.addEventListener('click', (event) => {
+            event.preventDefault();
+            showInfoSerie(serie);
+        });
+
+        totalSeasons += serie.getSeasons();
+        seriesList.appendChild(serieRow);
+    });
+
+    seriesTable.appendChild(seriesList); 
+
+    const averageSeasons = Math.round(totalSeasons / series.length);
+    const averageText: HTMLElement = document.createElement('p');
+    averageText.className = 'mt-3 avg-text';
+    averageText.textContent = `Average number of seasons: ${averageSeasons}`;
+    seriesTable.parentElement!.appendChild(averageText);
+}
+
+function showInfoSerie(serie: any): void {
+    const cardContainer: HTMLElement = document.getElementById('cardContainer')!;
+    const serieImage: HTMLImageElement = document.getElementById('serieImage') as HTMLImageElement;
+    const serieTitle: HTMLElement = document.getElementById('serieTitle')!;
+    const serieDescription: HTMLElement = document.getElementById('serieDescription')!;
+    const serieLink: HTMLAnchorElement = document.getElementById('serieLink') as HTMLAnchorElement;
+
+    serieImage.src = serie.getImage(); // URL de imagen desde web
+    serieImage.alt = `Image of ${serie.getName()}`;
+    serieTitle.textContent = serie.getName();
+    serieDescription.textContent = serie.getDescription();
+    serieLink.href = serie.getLink();
+
+    cardContainer.style.display = 'block';
+}
---
+++ src/script/main.js
@@ -0,0 +1,55 @@
+import { series } from './data.js';
+
+const seriesTable = document.getElementById('seriesTable');
+showSeries(series);
+
+function showSeries(series) {
+    const seriesList = document.createElement('tbody');
+    let totalSeasons = 0;
+
+    series.forEach(serie => {
+        const serieRow = document.createElement('tr');
+        serieRow.innerHTML = `
+            <td class="series-id">${serie.getId()}</td>
+            <td class="series-name">
+                <a href="#" class="serie-link">${serie.getName()}</a>
+            </td>
+            <td>${serie.getChannel()}</td>
+            <td>${serie.getSeasons()}</td>
+        `;
+
+        const linkElement = serieRow.querySelector('.serie-link');
+        linkElement.addEventListener('click', function (event) {
+            event.preventDefault();
+            showInfoSerie(serie);
+        });
+
+        totalSeasons += serie.getSeasons();
+        seriesList.appendChild(serieRow);
+    });
+
+    seriesTable.appendChild(seriesList);
+
+    const averageSeasons = Math.round(totalSeasons / series.length);
+    const averageText = document.createElement('p');
+    averageText.className = 'mt-3 avg-text';
+    averageText.textContent = `Average number of seasons: ${averageSeasons}`;
+    seriesTable.parentElement.appendChild(averageText);
+}
+
+function showInfoSerie(serie) {
+    const cardContainer = document.getElementById('cardContainer');
+    const serieImage = document.getElementById('serieImage');
+    const serieTitle = document.getElementById('serieTitle');
+    const serieDescription = document.getElementById('serieDescription');
+    const serieLink = document.getElementById('serieLink');
+
+    serieImage.src = serie.getImage(); 
+    serieImage.alt = `Image of ${serie.getName()}`;
+    serieTitle.textContent = serie.getName();
+    serieDescription.textContent = serie.getDescription();
+    serieLink.href = serie.getLink();
+    serieLink.target = '_blank'; 
+
+    cardContainer.style.display = 'block';
+}
